6e4c4cd3b75e8ba4fa09a10f602389e207d0baf8
Tomasz <tomaszboron2k@gmail.com>
2022-02-04 22:57:05 +0100

    create rwd for main site

--- wp-content/themes/twenty_twenty-two_child/front-page.php
+++ wp-content/themes/twenty_twenty-two_child/front-page.php
@@ -21,17 +21,25 @@
     </div>
     <img src="<?php bloginfo('stylesheet_directory'); ?>/img/image 1.png" alt="bars">
     <div class="bottom-cards">
-        <div class="card"></div>
+        <div class="card">
+        <h3> 1 </h3>
+        <p>Lorem, <span> ipsum </span> dolor sit amet consectetur adipisicing elit. Nam architecto temporibus iure minima non. Blanditiis enim quod tenetur dolorum doloribus illo illum voluptates dolor et! </p>
+        </div>
         <div class="card">
         <h3> 2 </h3>
         <p>Lorem, <span> ipsum </span> dolor sit amet consectetur adipisicing elit. Nam architecto temporibus iure minima non. Blanditiis enim quod tenetur dolorum doloribus illo illum voluptates dolor et! </p>
         </div>
-        <div class="card"></div>
+        <div class="card">
+        <h3> 3 </h3>
+        <p>Lorem, <span> ipsum </span> dolor sit amet consectetur adipisicing elit. Nam architecto temporibus iure minima non. Blanditiis enim quod tenetur dolorum doloribus illo illum voluptates dolor et! </p>
+        </div>
         <div class="card">
         <h3> 4 </h3>
         <p>Lorem, <span> ipsum </span> dolor sit amet consectetur adipisicing elit. Nam architecto temporibus iure minima non. Blanditiis enim quod tenetur dolorum doloribus illo illum voluptates dolor et! </p>
         </div>
         <div class="card">
+        <h3> 5 </h3>
+        <p>Lorem, <span> ipsum </span> dolor sit amet consectetur adipisicing elit. Nam architecto temporibus iure minima non. Blanditiis enim quod tenetur dolorum doloribus illo illum voluptates dolor et! </p>
         </div>
     </div>
 </div>
