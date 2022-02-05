30b38fb3d7cc018a76fa241d6ce4c4733e19a2e2
Tomasz <tomaszboron2k@gmail.com>
2022-02-05 13:06:29 +0100

    create employees list

--- wp-content/themes/twenty_twenty-two_child/functions.php
+++ wp-content/themes/twenty_twenty-two_child/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+add_filter('show_admin_bar', '__return_false');
+
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
 function task_register_styles(){
     $version = wp_get_theme()->get('Version');
     wp_enqueue_style('task-custom', get_stylesheet_directory_uri() . "/style.css", array(), $version, 'all');
@@ -8,9 +14,18 @@ function task_register_styles(){
 add_action('wp_enqueue_scripts', 'task_register_styles');
 
 function task_register_scripts(){
-    //wp_enqueue_script('cardiomed-jquery', "https://code.jquery.com/jquery-3.6.0.min.js", array(), "3.6.0", true);
+    wp_enqueue_script('task-loader', get_stylesheet_directory_uri() . "/js/loader.js", array(), "1.0", true);
 }
 
 add_action('wp_enqueue_scripts', 'task_register_scripts');
 
-add_filter('show_admin_bar', '__return_false');
+function my_acf_json_save_point( $path ) {
+    $path = get_stylesheet_directory() . '/acf-json';
+    return $path;
+}
+
+function my_acf_json_load_point( $paths ) {
+    unset($paths[0]);
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    return $paths;    
+}
